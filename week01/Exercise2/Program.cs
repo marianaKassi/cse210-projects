de97a5ad397dab9323f32f5627ae5e9bab05a04f
marianaKassi <Kassighislaine88@gmail.com>
2025-01-25 22:46:15 +0000

    Update Program.cs

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -5,5 +5,82 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
+
+
+    
+        Console.Write("Enter your percentage grade: ");
+        int percentage = Convert.ToInt32(Console.ReadLine());
+
+
+        string letter = "";
+        string sign = "";
+
+        
+        if (percentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (percentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (percentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (percentage >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        
+        if (percentage >= 70)
+        {
+            Console.WriteLine("Congratulations, you passed the course!");
+        }
+        else
+        {
+            Console.WriteLine("Keep trying for next time!");
+        }
+
+        
+        if (letter != "F") 
+        {
+            int lastDigit = percentage % 10;
+
+            if (lastDigit >= 7)
+            {
+                sign = "+";
+            }
+            else if (lastDigit < 3)
+            {
+                sign = "-";
+            }
+        }
+
+    
+        if (letter == "A" && sign == "+")
+        {
+            Console.WriteLine("Your grade is A.");
+        }
+        else if (letter == "A" && sign == "-")
+        {
+            Console.WriteLine("Your grade is A-.");
+        }
+        else if (letter == "F")
+        {
+            Console.WriteLine("Your grade is F.");
+        }
+        else
+        {
+            Console.WriteLine($"Your grade is {letter}{sign}.");
+        }
     }
 }
+
+
+    
