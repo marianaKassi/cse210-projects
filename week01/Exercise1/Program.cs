b8820bba4c8fcabac70bf266050a5e5f300eb861
marianaKassi <Kassighislaine88@gmail.com>
2025-01-25 22:17:53 +0000

    Update Program.cs

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,5 +5,12 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
+
+       Console.WriteLine("What is your first name? KASSI");
+
+       Console.WriteLine(" What is your last name? Mariana");
+
+
+Console.Write("Your name is KASSI, Mariana KASSI");
     }
 }
