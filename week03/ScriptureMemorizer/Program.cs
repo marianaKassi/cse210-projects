8ffac4215e89d9eac28761f89dd70c941c297466
marianaKassi <kassighislaine88@gmail.com>
2025-02-07 20:04:45 +0000

    added a comment

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -11,6 +11,8 @@ class Program
         Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
     
         Console.WriteLine("Hello World! This is the ScriptureMemorizer Project Mariana.");
+                Console.WriteLine("Hello World! This is the ScriptureMemorizer Project Mariana.");
+
 
     }
 }
