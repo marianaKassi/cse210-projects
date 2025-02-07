74a67baba2f7abf7438be259149e22d022ed6bd1
marianaKassi <kassighislaine88@gmail.com>
2025-02-07 21:06:53 +0000

    Added

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -11,6 +11,6 @@ class Program
        Console.WriteLine(" What is your last name? Mariana");
 
 
-Console.Write("Your name is KASSI, Mariana KASSI Ria" );
+Console.Write("Your name is KASSI,G Mariana KASSI");
     }
 }
